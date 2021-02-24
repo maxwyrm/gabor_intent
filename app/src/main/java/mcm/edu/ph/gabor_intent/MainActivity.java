6426ec13582304b3cc262dce6869875888b03ff0
Annie Gabor <kaidengabs@gmail.com>
2021-02-24 18:14:21 +0800

    Initial commit

---
+++ app/src/main/java/mcm/edu/ph/gabor_intent/MainActivity.java
@@ -0,0 +1,54 @@
+package mcm.edu.ph.gabor_intent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button next1 = findViewById(R.id.btnNextCon1);
+
+        next1.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        TextView display = findViewById(R.id.txtDisplay);
+        EditText input = findViewById(R.id.txtMonthValue);
+
+        Intent i = new Intent(MainActivity.this,Activity2.class);
+
+        int numberMonths = Integer.parseInt(String.valueOf(display.getText()));
+
+
+
+
+        switch (v.getId()) {
+            case R.id.btnNextCon1:
+
+                i.putExtra("INT_MONTHS", numberMonths);
+
+
+
+                startActivity(i);
+                break;
+
+        }
+
+
+
+
+    }
+}
